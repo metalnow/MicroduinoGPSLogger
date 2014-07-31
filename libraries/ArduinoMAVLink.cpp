e89c86b73f3e98c18e39944b889393a90675e1e3
metalnow <shaune.jan@gmail.com>
2014-07-31 16:04:59 +0800

    mavlink setmode and flyhere
    implement set mode and flyhere

--- libraries/ArduinoMAVLink.cpp
+++ libraries/ArduinoMAVLink.cpp
@@ -1,10 +1,207 @@
 #include "ArduinoMAVLink.h"
 #include "include/mavlink/v1.0/common/mavlink.h"        // Mavlink interface
+#include "include/mavlink/v1.0/ardupilotmega/ardupilotmega.h"
 
+char ArduinoMAVLink::_receive_buffer[_ML_MAX_BUFF]; 
+status_callback ArduinoMAVLink::cb_status;
 
 ArduinoMAVLink::ArduinoMAVLink(SoftwareSerial * serial)
 {
-  _serial = serial;
+  telemSwserial = serial;
+}
+
+ArduinoMAVLink::ArduinoMAVLink(HardwareSerial * serial)
+{
+  telemHwSerial = serial;
+}
+
+void ArduinoMAVLink::SetStatusCallback( status_callback callback )
+{
+  cb_status = callback;
+}
+
+#define HEART_BEAT_COMFIRM 2
+#define INITIAL_TIMEOUT 30000
+bool ArduinoMAVLink::Initialize()
+{
+  error = ML_ERR_NONE;
+  unsigned long timer = millis();
+  
+  Stream * _serial;
+  if ( telemSwserial )
+    _serial = telemSwserial;
+  else
+    _serial = telemHwSerial;
+    
+  mavlink_message_t msg;
+  mavlink_status_t status;
+   
+  uint8_t bufIdx = 0;
+  uint8_t hb_count = 0;
+  
+  while (hb_count < HEART_BEAT_COMFIRM && bufIdx < _ML_MAX_BUFF)
+  {  
+    unsigned long elapsed_time = millis() - timer;
+    StatusCallback( ML_INITIAL, elapsed_time );
+    if ( elapsed_time > INITIAL_TIMEOUT )      
+    {
+      error = ML_ERR_TIMEOUT;
+      return false;
+    }
+    
+    while (_serial->available() > 0 && bufIdx < _ML_MAX_BUFF) 
+    {
+      uint8_t c = _serial->read();
+      _receive_buffer[bufIdx++] = c;
+      
+      // Try to get a new message
+      if(mavlink_parse_char(MAVLINK_COMM_0, c, &msg, &status)) 
+      {
+        // Handle message    
+        switch(msg.msgid)
+        {
+          case MAVLINK_MSG_ID_HEARTBEAT:
+          {
+            mavlink_heartbeat_t hb;
+            memcpy( &hb, _receive_buffer, bufIdx );
+            if ( hb.type != MAV_TYPE_GCS )
+            {
+              if ( hb_count == 0 )
+              {
+                mavlinkversion = hb.mavlink_version;
+                type = hb.type;
+                autopilot = hb.autopilot;
+                sysid = msg.sysid;
+                compid = msg.compid;
+                recvpacketcount = msg.seq;
+              }
+              else
+              {
+                if ( sysid != msg.sysid || compid != msg.compid )
+                  hb_count--;
+              }
+              hb_count++;
+              bufIdx = 0;
+            }
+          }
+          break;
+          default:
+          //Do nothing
+          break;
+        }
+      }
+      // And get the next one
+    }
+  }
+  
+  return hb_count==HEART_BEAT_COMFIRM;  
+}
+
+void ArduinoMAVLink::StatusCallback( uint8_t status, uint32_t msg )
+{
+  if ( cb_status )
+    cb_status(status, msg);
+}
+
+// 0:Stabilize,1:Acro,2:AltHold,3:Auto,4:Guided,5:Loiter,6:RTL,7:Circle,9:Land,10:OF_Loiter,11:Drift,13:Sport
+void ArduinoMAVLink::SetMode( uint8_t fltMode )
+{
+  mavlink_set_mode_t mode;
+  
+  mode.target_system = sysid;
+  mode.base_mode = MAV_MODE_FLAG_CUSTOM_MODE_ENABLED;
+  mode.custom_mode = fltMode;  
+  
+  mavlink_message_t msg;
+	mavlink_msg_set_mode_encode( 255/*sysid*/, MAV_COMP_ID_MISSIONPLANNER/*compid*/, &msg, &mode);
+  
+  uint8_t buffer[MAVLINK_MAX_PACKET_LEN];
+  uint16_t len = mavlink_msg_to_send_buffer(buffer, &msg);
+  
+  Write( buffer, len );
+}
+
+void ArduinoMAVLink::FlyHere( double lat, double lon, double alt )
+{
+  mavlink_mission_item_t wp;
+  wp.target_system = sysid;
+  wp.target_component = compid; // MSG_NAMES.MISSION_ITEM
+
+  wp.command = MAV_CMD_NAV_WAYPOINT;
+
+  wp.current = 2; // 0 = no , 2 = guided mode
+  wp.autocontinue = 1;
+
+  wp.frame = MAV_FRAME_GLOBAL_RELATIVE_ALT;
+  wp.y = lon;
+  wp.x = lat;
+  wp.z = alt;
+
+  wp.param1 = 0;
+  wp.param2 = 0;
+  wp.param3 = 0;
+  wp.param4 = 0;
+
+  wp.seq = 0; // waypoint index at 0
+  
+  mavlink_message_t msg;
+  uint8_t buffer[MAVLINK_MAX_PACKET_LEN];
+  mavlink_msg_mission_item_encode(255/*sysid*/, MAV_COMP_ID_MISSIONPLANNER/*compid*/, &msg, &wp);
+  uint16_t len = mavlink_msg_to_send_buffer(buffer, &msg);
+  
+  Write( buffer, len );
+}
+
+void ArduinoMAVLink::Write( uint8_t * buffer, uint16_t length )
+{
+  Stream * _serial;
+  if ( telemSwserial )
+    _serial = telemSwserial;
+  else
+    _serial = telemHwSerial;
+  
+  _serial->write(buffer, length);
+}
+
+bool ArduinoMAVLink::Received()
+{
+  bool got = false;
+  Stream * _serial;
+  if ( telemSwserial )
+    _serial = telemSwserial;
+  else
+    _serial = telemHwSerial;
+    
+  mavlink_message_t msg;
+  mavlink_status_t status;
+      
+  while (_serial->available() > 0) 
+  {
+    uint8_t c = _serial->read();
+    
+    if(mavlink_parse_char(MAVLINK_COMM_0, c, &msg, &status)) 
+    {
+      // Handle message    
+      switch(msg.msgid)
+      {
+        case MAVLINK_MSG_ID_HEARTBEAT:
+        {
+        }
+        break;
+        case MAVLINK_MSG_ID_COMMAND_LONG:
+        // EXECUTE ACTION
+        break;
+        default:
+        //Do nothing
+        break;
+      }
+      
+      got = true;
+    }
+    // And get the next one
+  }
+  
+  return got;
 }
 
 /*
@@ -39,44 +236,5 @@ void loop() {
      comm_receive();
 }
 */
-
-
-void ArduinoMAVLink::comm_receive() 
-{
-/* 
-  mavlink_message_t msg;
-  mavlink_status_t status;
-  
-  // COMMUNICATION THROUGH EXTERNAL UART PORT (XBee serial)
-  
-  while(mySerial.available() > 0 ) 
-  {
-    uint8_t c = _serial->read();
-    // Try to get a new message
-    if(mavlink_parse_char(MAVLINK_COMM_0, c, &msg, &status)) {
-    // Handle message
-    
-      switch(msg.msgid)
-      {
-        case MAVLINK_MSG_ID_HEARTBEAT:
-        {
-          // E.g. read GCS heartbeat and go into
-          // comm lost mode if timer times out
-        }
-        break;
-        case MAVLINK_MSG_ID_COMMAND_LONG:
-        // EXECUTE ACTION
-        break;
-        default:
-        //Do nothing
-        break;
-      }
-    }
-    // And get the next one
-  }
-*/  
-}
-
-       
        
 
--- libraries/ArduinoMAVLink.h
+++ libraries/ArduinoMAVLink.h
@@ -1,14 +1,56 @@
+#include "Arduino.h"
 #include <SoftwareSerial.h>
+
+#define _ML_MAX_BUFF 64 // buffer size
+
+typedef enum
+{
+  ML_ERR_NONE = 0,
+  ML_ERR_TIMEOUT  = 1,
+} ML_Error;
+
+typedef enum
+{
+  ML_NONE = 0,
+  ML_INITIAL = 1,
+} ML_Status;
+
+typedef void (*status_callback) (uint8_t, uint32_t);
 
 class ArduinoMAVLink
 {
 public:  
   ArduinoMAVLink(SoftwareSerial * serial);
+  ArduinoMAVLink(HardwareSerial * serial);
   
-  void comm_receive();
+  void SetStatusCallback( status_callback callback );
+  
+  bool Initialize();
+  void FlyHere( double lat, double lon, double alt );  
+  void SetMode( uint8_t fltMode );
+  bool Received(); 
+  
 protected:
 
-  SoftwareSerial * _serial;  
+  SoftwareSerial * telemSwserial;
+  HardwareSerial * telemHwSerial;
+  
+private:
+  void StatusCallback( uint8_t status, uint32_t msg );
+  void Write( uint8_t * buffer, uint16_t length );
+
+  static char _receive_buffer[_ML_MAX_BUFF];   
+  
+  static status_callback cb_status;
+  
+  uint8_t mavlinkversion;
+  uint8_t type;
+  uint8_t autopilot;
+  uint8_t sysid;
+  uint8_t compid;
+  uint8_t recvpacketcount;
+  
+  uint8_t error;
 };
 
 
